7a4423f9492bf8b9feca66c305d7e9da1d13407b
Ronald Marques <ronaldpmarques01@gmail.com>
2024-12-16 13:41:04 -0300

    Criação da rota de atualização de refeição

--- app.py
+++ app.py
@@ -60,6 +60,39 @@ def read_meal(meal_id):
     })
 
 
+@app.route("/meal/<int:meal_id>", methods=["PATCH"])
+def update_meal(meal_id):
+    data = request.json
+    meal = Meal.query.get(meal_id)
+
+    if not data:
+        return jsonify({"message": "Payload vazio"}), 400
+
+    if not meal:
+        return jsonify({
+            "message": "Refeicao nao encontrada"
+        }), 404
+
+    if data.get("name"):
+        meal.name = data.get("name")
+
+    if data.get("description"):
+        meal.description = data.get("description")
+
+    if data.get("date") and is_valid_datetime(data.get("date")):
+        meal.date = data.get("date")
+
+    if data.get("off_diet") is not None:
+        meal.off_diet = data.get("off_diet")
+
+    db.session.commit()
+
+    return jsonify({
+        "meal_id": meal.id,
+        "message": f"Refeicao {meal.id} atualizada com sucesso"
+    })
+
+
 @app.route("/meal/<int:meal_id>", methods=["DELETE"])
 def delete_meal(meal_id):
     meal = Meal.query.get(meal_id)
